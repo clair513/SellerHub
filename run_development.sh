bc55154fff97838a723876bfda2c0c7aafabcbe5
Random Nerd <32665974+clair513@users.noreply.github.com>
2018-12-15 19:57:20 +0530

    App initiation script

---
+++ run_development.sh
@@ -0,0 +1,7 @@
+"""
+[NOT IN USE] For MacOS/Linux OS. For Windows, will have to use Powershell for creating batch file.
+"""
+export FLASK_APP = app.py
+flask db init
+flask db migrate
+flask db upgrade
